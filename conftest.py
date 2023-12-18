ebb1c9107ced04edc98378f0886c1fd2dc9c397c
Stepan <timoshenko.stepa@mail.ru>
2023-12-18 20:22:23 +0700

    Removed the language fixture

--- conftest.py
+++ conftest.py
@@ -1,26 +1,21 @@
 import pytest
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                     help="Enter the ISO 639-1 language code")
+                     help="Enter the language code")
 
 @pytest.fixture(scope="function")
-def browser():
-    #Returns chrome browser
-    browser = webdriver.Chrome()
+def browser(request):
+    """Returns the Chrome browser in the selected language"""
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+
+    browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(10)
     yield browser
     browser.quit()
 
-@pytest.fixture(scope="function")
-def language(request):
-    #Returns the ISO 639-1 language code
-    language_code = str(request.config.getoption("language"))
-    #Check that the language_code is two letters
-    if language_code.isalpha() and len(language_code) == 2:
-        return language_code
-    else:
-        raise pytest.UsageError("--language should be the ISO 639-1 language code")
 
--- test_items.py
+++ test_items.py
@@ -2,11 +2,10 @@ import pytest
 from selenium.webdriver.common.by import By
 
 
-def test_check_add_to_basket(browser, language):
-    #Checking if there is an button add to basket
-    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
-    browser.get(link)
+link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+def test_check_add_to_basket(browser):
+    """Checking if there is an button add to basket"""
+    browser.get(link)
     result = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
     assert len(result) > 0, 'Button not found'
-    
